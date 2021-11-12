08c3a09fb40527064deff6f233e63a72df812e17
Delevo <max.on777c@gmail.com>
2021-11-12 01:39:54 +0100

    t4 input processing added

---
+++ baca/t4.cpp
@@ -0,0 +1,39 @@
+// Maksim Zdobnikau
+// Kostkowy poker
+
+// baca submit -f file.cpp -t 8 -l "C++ z obsluga plikow"
+
+
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    char playerA = 'a';
+    char playerB = 'b';
+    char playerC = 'c';
+    char playerD = 'd';
+    char playerE = 'e';
+
+    float wonA = 0;
+    float wonB = 0;
+    float wonC = 0;
+    float wonD = 0;
+    float wonE = 0;
+
+    float drawsA = 0;
+    float drawsB = 0;
+    float drawsC = 0;
+    float drawsD = 0;
+    float drawsE = 0;
+
+    float lostA = 0;
+    float lostB = 0;
+    float lostC = 0;
+    float lostD = 0;
+    float lostE = 0;
+    return 0;
+}
