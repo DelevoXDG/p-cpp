636560a660331b639cdb682efa54a3443f20b91f
Delevo <max.on777c@gmail.com>
2021-11-12 06:04:18 +0100

    changes to t4, new tests

--- baca/compare.sh
+++ baca/compare.sh
@@ -8,6 +8,6 @@ for (( i=1; i <= $max; ++i))
 do
 echo -e "test${i}:"
 ./exe1 < Tests/PokerKostkowy/poker.0${i}.in > tests2/1-test${i}.out
-./main < Tests/PokerKostkowy/poker.0${i}.in > tests2/2-test${i}.out
+./exe2 < Tests/PokerKostkowy/poker.0${i}.in > tests2/2-test${i}.out
 diff --color tests2/1-test${i}.out tests2/2-test${i}.out
 done
