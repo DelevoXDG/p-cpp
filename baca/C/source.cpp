9c37eb99187004062d7c38669396964aa40484fe
Delevo <max.on777c@gmail.com>
2021-12-02 18:15:33 +0100

    Union works

--- baca/C/source.cpp
+++ baca/C/source.cpp
@@ -43,12 +43,31 @@ void BasicSort(int in[], int arrSize) {
 }
 
 int size(int in[]) {
-	int i = 0;
-	while (in[i] != -1)
-		++i;
-	return i;
+	int n = 0;
+	while (in[n] != -1)
+		n++;
+	return n;
 }
 
+void AddNoSort(int num, int out[])
+{
+	int in[] = { num };
+	int n = 0;
+	bool isAddable = false;
+	if (num >= 1 && num <= 4095)
+	{
+		isAddable = true;
+	}
+	while ((out[n] != -1))
+	{
+		if (out[n++] == num) isAddable = false;
+	}
+	if (isAddable) {
+		out[n] = num;
+		n++;
+		out[n] = -1;
+	}
+}
 
 //SECTION MAIN FUNCTIONS
 void Add(int num, int out[])
@@ -96,60 +115,28 @@ void Create(int n, int in[], int out[])
 		BasicSort(out, o);
 	}
 	out[o] = -1;
-
-
 }
-
-/*
-int main(int argc, const char** argv)
+void Union(int* A, int* B, int* out)
 {
-
-
-	char q, D[100];
-	int n, m;
-	int mini, maxi, card;
-	double a, h;
-	int A[20000], B[5000], C[5000];
-
-	std::cin >> q;
-	if (q == 'A') {//Add
-		int x = 0;
-		while (true) {
-			std::cin >> *(A + x);
-			if (*(A + x++) == -1)
-				break;
-		}
-		std::cin >> m;
-		std::cout << m << "\n";
-		Add(m, A);
-		x = 0;
-		while (*(A + x) != -1)
-			std::cout << *(A + x++) << " ";
-		std::cout << *(A + x) << "\n";
-
-	}//Add
-	else
-		if (q == 'B') {//Create
-			std::cin >> n;
-			for (int i = 0; i < n; ++i)
-				std::cin >> A[i];
-			Create(n, A, B);
-			for (int i = 0; i < n; ++i)
-				std::cout << A[i] << " ";
-			std::cout << "\n";
-			int x = 0;
-			while (*(B + x) != -1)
-				std::cout << *(B + x++) << " ";
-			std::cout << *(B + x);
-		}
-	return 0;
+	int sizeA = size(A);
+	int sizeB = size(B);
+	Create(sizeA, A, out);
+	for (int i = 0; i < sizeB; i++)
+		AddNoSort(B[i], out);
+	int o = size(out);
+	BasicSort(out, o);
 }
-*/
 
+void Intersection(int* A, int* B, int* out)
+{
+	int sizeA = size(A);
+	int sizeB = size(B);
+	for (int i = 0; i < sizeA; i++)
+	{
 
+	}
 
-
-
+}
 
 
 
