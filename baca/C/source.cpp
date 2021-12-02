4fc4e208578e84b07e57bb70c346c7ae7bc6e187
Delevo <max.on777c@gmail.com>
2021-12-02 16:27:02 +0100

    02 works

--- baca/C/source.cpp
+++ baca/C/source.cpp
@@ -1,15 +1,27 @@
 // Maksim Zdobnikau
-// https://baca.ii.uj.edu.pl/p12021/
-// baca submit --file source.cpp --rename source.cpp --task-id 7 --language "C++" --zip
 
-//#include "PPZ.h"
 #include <iostream>
 using std::cin; using std::cout; using std::endl; using std::string;
 
 //SECTION TODO
-//TODO RemoveDuplicates
-//TODO CREATE
-
+//[X] RemoveDuplicates
+//[X] CREATE
+//[ ] Add
+//[ ] Create
+//[ ] Union
+//[ ] Intersection
+//[ ] Difference
+//[ ] Symmetric
+//[ ] Complement;
+//[ ] Subset;
+//[ ] Equal;
+//[ ] Empty;
+//[ ] Nonempty;
+//[ ] Element;
+//[ ] Arithmetic;
+//[ ] Harmonic;
+//[ ] MinMax;
+//[ ] Cardinality
 
 
 //SECTION HELPER FUNCTIONS	
@@ -30,21 +42,37 @@ void BasicSort(int in[], int arrSize) {
 	}
 }
 
-int RemoveDuplicatesSorted(int arr[], int arrSize)
-{
-
+int size(int in[]) {
 	int i = 0;
-	int j = 0;
-	if (arrSize == 0) return 0;
-	for (j = 1; j < arrSize; j++)
-	{
-		if (arr[j] != arr[i]) {
-			arr[++i] = arr[j];
-		}
-	}
-	return i + 1;
+	while (in[i] != -1)
+		++i;
+	return i;
 }
+
+
 //SECTION MAIN FUNCTIONS
+void Add(int num, int out[])
+{
+	int in[] = { num };
+	int n = 0;
+	bool isAddable = false;
+	if (num >= 1 && num <= 4095)
+	{
+		isAddable = true;
+	}
+	while ((out[n] != -1))
+	{
+		if (out[n++] == num) isAddable = false;
+	}
+	if (isAddable) {
+		out[n] = num;
+		n++;
+		out[n] = -1;
+	}
+	if (n > 1) BasicSort(out, n);
+
+}
+
 void Create(int n, int in[], int out[])
 {
 	int i = 0; int o = 0;
@@ -66,7 +94,6 @@ void Create(int n, int in[], int out[])
 	if (o > 1)
 	{
 		BasicSort(out, o);
-		//o = RemoveDuplicatesSorted(out, o);
 	}
 	out[o] = -1;
 
@@ -85,19 +112,36 @@ int main(int argc, const char** argv)
 	int A[20000], B[5000], C[5000];
 
 	std::cin >> q;
-	if (q == 'B') {//Create
-		std::cin >> n;
-		for (int i = 0; i < n; ++i)
-			std::cin >> A[i];
-		Create(n, A, B);
-		for (int i = 0; i < n; ++i)
-			std::cout << A[i] << " ";
-		std::cout << "\n";
+	if (q == 'A') {//Add
 		int x = 0;
-		while (*(B + x) != -1)
-			std::cout << *(B + x++) << " ";
-		std::cout << *(B + x);
-	}
+		while (true) {
+			std::cin >> *(A + x);
+			if (*(A + x++) == -1)
+				break;
+		}
+		std::cin >> m;
+		std::cout << m << "\n";
+		Add(m, A);
+		x = 0;
+		while (*(A + x) != -1)
+			std::cout << *(A + x++) << " ";
+		std::cout << *(A + x) << "\n";
+
+	}//Add
+	else
+		if (q == 'B') {//Create
+			std::cin >> n;
+			for (int i = 0; i < n; ++i)
+				std::cin >> A[i];
+			Create(n, A, B);
+			for (int i = 0; i < n; ++i)
+				std::cout << A[i] << " ";
+			std::cout << "\n";
+			int x = 0;
+			while (*(B + x) != -1)
+				std::cout << *(B + x++) << " ";
+			std::cout << *(B + x);
+		}
 	return 0;
 }
 */
