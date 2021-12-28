d9de9dc1b3594ac957ed33dd95afc03549852e46
Delevo <max.on777c@gmail.com>
2021-12-28 01:17:18 +0100

    Pre-Changes

--- baca/E/main.cpp
+++ baca/E/main.cpp
@@ -2,34 +2,52 @@
 
 #include <iostream>
 
+//#define NDEBUG // toggle Assert()
+#include <cassert>
+
 using std::cin; using std::cout; using std::endl; using std::string;
 
 #define nullptr NULL
 typedef unsigned char uint8;
 typedef unsigned short int uint16;
+typedef unsigned long long uint64;
+
+//SECTION TODO LIST
+//[X] Add enum switch maybe? (no)
+//[X] Remove back pointers
+//[ ] Implement get(),set() functions
+//[ ] Implement top-level interface
+
+//[ ] Change opSelection to switch 
 
 namespace IOS {
 
-	void Output() { std::cout << endl; }
 
-	template<typename FirstT, typename ... ArgsT>
-	void Output(FirstT first, ArgsT&... args) {
-		std::cout << first;
-		Output(args...);
-	}
+	// 	void Output() { std::cout << endl; }
 
-	void Input() { ; }
+	// 	template<typename FirstT, typename ... ArgsT>
+	// 	void Output(FirstT first, ArgsT&... args) {
+	// 		std::cout << first;
+	// 		Output(args...);
+	// 	}
 
-	template <typename FirstT, typename ... ArgsT>
-	void Input(FirstT& first, ArgsT&...args) {
-		std::cin >> first;
-		Input(args...);
-	}
+	// 	void Input() { ; }
+
+	// 	template <typename FirstT, typename ... ArgsT>
+	// 	void Input(FirstT& first, ArgsT&...args) {
+	// 		std::cin >> first;
+	// 		Input(args...);
+	// 	}
 
 	void PrintError() {
 		std::cout << "error" << endl;
 	}
 }
+
+// static void assert(bool condition) {
+// 	// try
+// 	if (!condition);
+// }
 
 //SECTION STORAGE STRUCTS
 namespace Storage_NS {
@@ -40,65 +58,149 @@ namespace Storage_NS {
 	const uint8 MAX_RACK = 128;
 	const uint8 MAX_WAREHOUSE = 128;
 	const uint8 MAX_STORAGE = 128;
+	struct Shelf;
+	uint64 goods_sum = 0;
+
+	enum PlaceState : bool {
+		empty = 0 //Used to set empty label
+	};
+
+	struct Link {
+		Shelf* parent[MAX_PLACE];
+	};
 
 	struct Place {
 
-		uint16 goods; //Number of goods stored in Place upto 65535
-		uint8 label;
-		bool isLabeled;
 
-		Place() : goods(0), label(0), isLabeled(false) {}
+		uint16 num_goods; //Number of goods stored in Place upto 65535
 
-		void Clear() {
-			goods = 0;
-			label = 0;
-			isLabeled = false;
-		}
-		void SetValue(const uint16 ngoods, const bool nisLabeled, const uint8 nlabel) {
-			goods = ngoods;
-			isLabeled = nisLabeled;
-			label = nlabel;
-		}
-		void Put(const uint16 add) {
-			goods = (goods + add < MAX_GOODS) ? (goods + add) : MAX_GOODS;
-		}
-		void Pop(const uint16 sub) {
-			goods = (goods - sub > 0) ? (goods - sub) : 0;
-		}
-		void Move(Place& target, uint16 a) {
-			a = (a < goods) ? a : goods;
-			a = (a + target.goods < MAX_GOODS) ? a : (MAX_GOODS - target.goods);
 
-			goods -= a;
-			target.goods += a;
+		uint8 label;     //  isLabeled ==> label in [0..99]
+		bool isLabeled;  // !isLabeled ==> label undefined
+		// uint8 id;
+		// Shelf& parent;
+
+		Place() : num_goods(0), isLabeled(false), label(0) {}
+
+		void SetLabel(uint8 label) {
+			assert(!this->isLabeled); // TODO
+			this->label = label;
+			isLabeled = true;
 		}
+
+		uint8 GetLabel() {
+			assert(this->isLabeled);
+			return this->label;
+		}
+
+		bool CheckLabeled() {
+			return this->isLabeled;
+		}
+
+		// Place() : goods(0), label(0), isLabeled(false), parent(nullptr) {}
+		void Clear();
+		void SetValue(const uint16, const uint8);
+		void SetValue(const uint16, const PlaceState);
+		void Put(uint16);
+		void Pop(uint16);
+		void Move(Place&, uint16);
+		uint16 get_num_goods();
+
+		bool invariant();
 	};
-
 	struct Shelf {
 		uint8 size; //Number of Places stored in Shelf upto 128
-		Place m_place[MAX_PLACE];
+		Place 	m_place[MAX_PLACE];
+		uint64 num_goods;
+		//uint8 id;
 
-		Shelf() : size(0) {}
+		Shelf() {
+			size = 0;
+			num_goods = 0;
+			// 	for (uint8 i = 0; i < MAX_PLACE; i++) {
+			// 		m_place[i].id = i;
+			// 	}
+			// 
+		}
+		inline void SetSize(const uint8);
+		uint64 GetSum();
 
-		void SetSize(const uint8 ns0) { //BaCa won't allow n3w keyworc
-			uint8 a = size < ns0 ? size : ns0;
-			uint8 b = size > ns0 ? size : ns0;
+		Place& GetPlace(const uint8 p) {
+			return m_place[p];
+		}
+	};
 
-			for (size_t i = a; i < b; i++) {
-				m_place[i].SetValue(0, false, 0);
-			}
-			size = ns0;
+	struct Rack;
+	struct Warehouse;
+	struct Storage;
+
+	void Place::Clear() {
+		num_goods = 0;
+		label = 0;
+		isLabeled = false;
+	}
+	void Place::SetValue(const uint16 ngoods, const uint8 nlabel) {
+		num_goods = ngoods;
+		isLabeled = true;
+		label = nlabel;
+
+		goods_sum += (ngoods - num_goods);
+	}
+	void Place::SetValue(const uint16 ngoods, PlaceState state) {
+		if (state == empty) {
+			num_goods = ngoods;
+			isLabeled = false;
 		}
 
-	};
+		goods_sum += (ngoods - num_goods);
+	}
+
+	void Place::Put(uint16 add) {
+		add = (num_goods + add <= MAX_GOODS) ? add : MAX_GOODS - num_goods;
+		num_goods += add;
+		// parent[id].sum += num_goods;
+		goods_sum += num_goods;
+	}
+	void Place::Pop(uint16 sub) {
+		sub = (num_goods - sub > 0) ? sub : num_goods;
+		num_goods -= sub;
+		goods_sum -= sub;
+	}
+	void Place::Move(Place& target, uint16 a) {
+		a = (a < num_goods) ? a : num_goods;
+		a = (a + target.num_goods < MAX_GOODS) ? a : (MAX_GOODS - target.num_goods);
+		num_goods -= a;
+		target.num_goods += a;
+	}
+	uint16 Place::get_num_goods() {
+		return num_goods;
+	}
+
+	bool Place::invariant() {
+		return this->num_goods <= MAX_GOODS; //&&
+	}
+
+
+	void Shelf::SetSize(const uint8 ns0) { //BaCa won't allow n3w keyword
+		uint8 a = size < ns0 ? size : ns0;
+		uint8 b = size > ns0 ? size : ns0;
+
+		for (size_t i = a; i < b; i++) {
+			m_place[i].SetValue(0, empty);
+		}
+		size = ns0;
+	}
+	uint64 Shelf::GetSum() {
+		return num_goods;
+	}
+
 
 	struct Rack {
 
 		uint8 size; //Number of Shelves stored in Rack upto 128 
 		Shelf m_shelf[MAX_SHELF];
-
-
-		Rack() : size(0) {}
+		uint64 sum;
+		Rack() : size(0) {	}
 
 		void SetSize(const uint8 ns0, const uint8 ns1) {
 			uint8 a = size < ns0 ? size : ns0;
@@ -108,6 +210,9 @@ namespace Storage_NS {
 				m_shelf[i].SetSize(ns1);
 			}
 			size = ns0;
+		}
+		uint64 GetSum() {
+			return sum;
 		}
 	};
 
@@ -180,7 +285,12 @@ namespace Storage_NS {
 		}
 
 		void Fill(const uint8 w, const uint8 r, const uint8 s, const uint8 p, uint8 a) {
-			a = a <= MAX_GOODS ? a : MAX_GOODS;
+			a = a < MAX_GOODS ? a : MAX_GOODS;
+			//warehouse - i
+			//shelf - k
+			//rack - j
+			//place - l
+
 			size = w;
 			for (int i = 0; i < w; i++) {
 				m_warehouse[i].size = r;
@@ -189,35 +299,43 @@ namespace Storage_NS {
 					for (int k = 0; k < s; k++) {
 						m_warehouse[i].m_rack[j].m_shelf[k].size = p;
 						for (int l = 0; l < p; l++) {
-							m_warehouse[i].m_rack[j].m_shelf[k].m_place[l].SetValue(a, false, 0);
+							m_warehouse[i].m_rack[j].m_shelf[k].m_place[l].SetValue(a, empty);
 						}
 					}
 				}
 				m_warehouse[i].m_handyShelf.size = p;
-				for (int l = 0; l < p; i++) {
-					m_warehouse[i].m_handyShelf.m_place[l].SetValue(a, false, 0);
+				for (int l = 0; l < p; l++) {
+					m_warehouse[i].m_handyShelf.m_place[l].isLabeled = false;
+					m_warehouse[i].m_handyShelf.m_place[l].num_goods = a;
 				}
 			}
-			m_handyRack.size = r;
-			for (int i = 0; i < r; i++) {
-				m_handyRack.m_shelf[i].size = s;
-				for (int j = 0; j < p; j++)
-					m_handyRack.m_shelf[i].m_place[j].SetValue(a, false, 0);
+			m_handyRack.size = s;
+			for (int k = 0; k < s; k++) {
+				m_handyRack.m_shelf[k].size = p;
+				for (int l = 0; l < p; l++) {
+					m_handyRack.m_shelf[k].m_place[l].isLabeled = false;
+					m_handyRack.m_shelf[k].m_place[l].num_goods = a;
+				}
 			}
 			m_handyShelf.size = p;
-			for (int i = 0; i < p; i++) {
-				m_handyShelf.m_place[i].SetValue(a, false, 0);
+			for (int l = 0; l < m_handyShelf.size; l++) {
+				m_handyShelf.m_place[l].isLabeled = false;
+				m_handyShelf.m_place[l].num_goods = a;
 			}
 		}
 	};
-
 }
 using namespace Storage_NS;
+
+// struct store {
+// 	static Storage storage;
+// }
 
 //SECTION MAIN
 int main(int argc, const char** argv)
 {
 	static Storage storage;
+	//static Storage special_storage(, num_racks=1);
 
 	int w = 0;
 	int r = 0;
@@ -236,7 +354,7 @@ int main(int argc, const char** argv)
 
 	while (opSelection != "END")
 	{
-		IOS::Input(opSelection);
+		cin >> opSelection;
 		string OP = opSelection.find('-') != std::string::npos ?
 			opSelection.substr(0, opSelection.find('-')) : opSelection;
 
@@ -336,7 +454,17 @@ int main(int argc, const char** argv)
 				if (storage.ReadAndCheck(0, 0, &s, &p, &a))
 					storage.m_handyRack.m_shelf[s].m_place[p].Move(
 						storage.m_handyShelf.m_place[p], a);
+			}
+			else if (OP == "GET")
+			{
+				if (arg == "E") { //Note GET-E
+					cout << goods_sum << endl;
+				}
 
+				else if (arg == "SW") {
+					if (storage.ReadAndCheck(&w, &r, &s))
+						cout << storage.m_warehouse[w].m_rack[r].m_shelf[s].GetSum() << endl;
+				}
 			}
 
 	}
