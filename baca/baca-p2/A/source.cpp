9c949a901fff6f97c5ec0c6889627b1264277a31
Delevo <max.on777c@gmail.com>
2022-03-18 19:50:52 +0100

    Finished baca-p2/a

--- baca/baca-p2/A/source.cpp
+++ baca/baca-p2/A/source.cpp
@@ -1,6 +1,5 @@
 //Maksim Zdobnikau
 
-
 // SECTION DECLARATIONS
 void Implace(char*, int*);
 void Insert(char*, int*);
@@ -13,7 +12,7 @@ bool Disjoint(int, int);
 bool Conjunctive(int, int);
 bool Equality(int, int);
 bool Inclusion(int, int);
-bool Union(int, int, int*);
+void Union(int, int, int*);
 void Intersection(int, int, int*);
 void Symmetric(int, int, int*);
 void Difference(int, int, int*);
@@ -23,8 +22,6 @@ bool LessThen(int, int);
 bool LessEqual(int, int);
 bool GreatEqual(int, int);
 bool GreatThen(int, int);
-int cstr_Length(char* const);
-
 // SECTION NOTEPAD
 // general idea: 
 // int 32 bit == 32 possible sets 
@@ -38,7 +35,8 @@ int Set_Idx_Parse(char* const cstr, const int local_i) { // Parses cstr to decim
 	// bitwise set (0|001 0000 0000 0000 0000 0000 0000 0000) == (11100, )
 	// bitwise set (0|100 0000 0000 0000 0000 0000 0000 0000) == (11110, )
 	// bitwise set (1|000 0000 0000 0000 0000 0000 0000 0000) == (11111, ) 
-	// FIX overflow ^   - sign 
+	// OK  overflow ^
+	// bitwise set (0|001 0000 0000 0001 0000 0000 0000 0000) == (11100, 10000)
 
 	if (*(cstr + local_i + 0) == '1') { Dec_Int = Dec_Int << 16; }
 	if (*(cstr + local_i + 1) == '1') { Dec_Int = Dec_Int << 8; }
@@ -48,27 +46,48 @@ int Set_Idx_Parse(char* const cstr, const int local_i) { // Parses cstr to decim
 
 	return Dec_Int;
 }
+int Lexicographically_Bigger(int A, int B) {
+	if (A < 0 || B < 0) { //Taking care of sign 
+		if (B < 0 && A > 0) 	return -1; 		// -1	A < B
+		if (B == A) 			return 0;  				//  0  	A == B
+		if (A < 0 && B > 0) 	return 1;		// 	1 	A > B
+	}
+	else {
+		if (A < B)				return -1;
+		if (A == B) 			return 0;
+		if (A > B) 				return 1;
+	}
+	return 0;
+}
 // SECTION PROBLEM FUNCTIONS
 void Insert(char* cstr, int* set) {
 	static int i = 0;
-	if (*(cstr + i) != '\0') { //Loop
-		if (*(cstr + i) == ' ') { i++; }
-		else {
-			*set = *set | Set_Idx_Parse(cstr, i); //Getting set index
-			i += 5;
-		}
-		Insert(cstr, set);
+	if (*(cstr + i) == '\0') { i = 0; return; } // 	Loop exit condition
+	else if (*(cstr + i) == ' ') { i++; }
+	else {
+		*set = *set | Set_Idx_Parse(cstr, i); 	//	Bitwise union: seq index added to set
+		i += 5;
 	}
-	else { i = 0; } //Loop ends
+	Insert(cstr, set);
 }
 void Emplace(char* cstr, int* set) {
 	*set = 0;
 	Insert(cstr, set);
 }
+void Erase(char* cstr, int* set) {
+	static int i = 0;
+	if (*(cstr + i) == '\0') { i = 0; return; } 	// 	Loop exit condition
+	else if (*(cstr + i) == ' ') { i++; }
+	else {
+		*set = *set & (~Set_Idx_Parse(cstr, i)); 	//	Bitwise A\{x}= A&~x
+		i += 5;
+	}
+	Erase(cstr, set);
+}
 void Print(int set, char* out) {
-	static int seq_idx = 31; //Descending order; Starting from (11111) = 1|000 0000 0000 0000 0000 0000 0000 0000
-	static int str_i = 0;
-	if (set == 0) { //empty set
+	static int seq_idx = 31; 	//	Descending order; Starting from (11111) = 1|000 0000 0000 0000 0000 0000 0000 0000
+	static int str_i = 0; 		//	current idx of destination char array
+	if (set == 0) { 			//	empty set
 		seq_idx = 31;
 		str_i = 0;
 		*(out + 0) = 'e';
@@ -79,7 +98,7 @@ void Print(int set, char* out) {
 		*(out + 5) = '\0';
 		return;
 	}
-	else if (seq_idx < 0) { //Loop ends
+	else if (seq_idx < 0) { 	// Loop exit condition
 		*(out + str_i) = '\0';
 		seq_idx = 31;
 		str_i = 0;
@@ -96,4 +115,111 @@ void Print(int set, char* out) {
 	}
 	seq_idx--;
 	Print(set, out);
+}
+void Complement(int input_set, int* set) {
+	*set = ~input_set;
+}
+bool Emptiness(int set) {
+	return set == 0;
+}
+bool Nonempty(int set) {
+	return set != 0;
+}
+bool Member(char* cstr, int set) {
+	static int i = 0;
+	if (*(cstr + i) == '\0') { i = 0; } 		// 	Loop exit condition
+	else if (*(cstr + i) == ' ') { i++; }
+	else {
+		bool result = set & Set_Idx_Parse(cstr, i); 	//	Intersection of set and singleton of seq
+		i = 0;
+		return result;
+	}
+	return Member(cstr, set);
+}
+bool Disjoint(int A, int B) {
+	return !(A & B);
+}
+bool Conjunctive(int A, int B) {
+	return (A & B);
+}
+bool Equality(int A, int B) {
+	return A == B;
+}
+bool Inclusion(int A, int B) {
+	return (A | B) == B;
+}
+void Union(int A, int B, int* C) {
+	*C = A | B;
+}
+void Intersection(int A, int B, int* C) {
+	*C = A & B;
+}
+void Symmetric(int A, int B, int* C) {
+	*C = (A & ~B) | (B & ~A);
+}
+void Difference(int A, int B, int* C) {
+	*C = (A & ~B);
+}
+int Cardinality(int set) { // Number of 1s int bitwise set representation 
+	static int i = 31;
+
+	if (i < 0) { // Loop exit condition
+		i = 31;
+		return 0;
+	}
+	if (set & (1 << i)) {
+		i--;
+		return Cardinality(set) + 1;
+	}
+	else i--;
+	return Cardinality(set);
+
+}
+bool LessThen(int A, int B) { // A < B ?
+	int cardA = Cardinality(A);
+	int cardB = Cardinality(B);
+	if (cardA == cardB) {
+		// int lexbigger = Lexicographically_Bigger(A, B);
+		// if (lexbigger == -1) 	return true;
+		// if (lexbigger == 0) 	return false;
+		// if (lexbigger == 1)		return false;
+		return A < B;
+	}
+	return cardA < cardB;
+}
+bool LessEqual(int A, int B) { // A <= B ?
+	int cardA = Cardinality(A);
+	int cardB = Cardinality(B);
+	if (cardA == cardB) {
+		// int lexbigger = Lexicographically_Bigger(A, B);
+		// if (lexbigger == -1) 	return true;
+		// if (lexbigger == 0) 	return true;
+		// if (lexbigger == 1)		return false;
+		return A <= B;
+	}
+	return cardA < cardB;
+}
+bool GreatEqual(int A, int B) { // A >= B ?
+	int cardA = Cardinality(A);
+	int cardB = Cardinality(B);
+	if (cardA == cardB) {
+		// int lexbigger = Lexicographically_Bigger(A, B);
+		// if (lexbigger == -1) 	return false;
+		// if (lexbigger == 0) 	return true;
+		// if (lexbigger == 1)		return true;
+		return A >= B;
+	}
+	return cardA >= cardB;
+}
+bool GreatThen(int A, int B) { // A > B ?
+	int cardA = Cardinality(A);
+	int cardB = Cardinality(B);
+	if (cardA == cardB) {
+		// int lexbigger = Lexicographically_Bigger(A, B);
+		// if (lexbigger == -1) 	return false;
+		// if (lexbigger == 0) 	return false;
+		// if (lexbigger == 1)		return true;
+		return A > B;
+	}
+	return cardA > cardB;
 }
